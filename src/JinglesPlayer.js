16eb2664f717ef24d99b09a085736d946acdf9ab
Yu Ling Cheng <cyuling23@gmail.com>
2024-03-11 21:06:54 +0000

    Add Ace

--- src/JinglesPlayer.js
+++ src/JinglesPlayer.js
@@ -3,6 +3,7 @@ import JinglePlayer from "./JinglePlayer";
 export default function JinglesPlayer(props) {
   return (
     <div className="jingles-player">
+      <JinglePlayer title="Ace!" fileName="audio/ace.mp3" />
       <JinglePlayer title="Block!" fileName="audio/monsterblock.mp3" />
       <JinglePlayer title="Spike!" fileName="audio/herecomestheboom.mp3" />
     </div>
